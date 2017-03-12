30cb98b8a4e2a03d0ba968ad5ca739841f5cb257
Alexandru Cristian <alex.cristian@drexel.edu>
2017-03-12 15:24:24 -0400

    Fixed Azure port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.port || 8080;
 // Lets Node serve files from the current directory.
 app.use(express.static('./Public/'));
 
-// Set Node to listen for incoming connections on port 80
-app.listen(80,function(){
+// Set Node to listen for incoming connections on port 8080
+app.listen(port,function(){
  console.log("Server Running");
 });
